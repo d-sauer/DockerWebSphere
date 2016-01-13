0719a8ae3504236fe6065a9665943cfd6ac2a666
Davor Sauer <davor@backbase.com>
2016-01-13 10:47:29 +0100

    Enable adminCenter

--- build-and-run-docker.sh
+++ build-and-run-docker.sh
@@ -1,3 +1,5 @@
-docker -D build -f ./websphere.docker -t registry.backbase.com/websphere-base .
+docker -D build -f ./websphere.docker -t registry.backbase.com/websphere-base:latest .
 
 docker-compose --verbose -f ./websphere.compose up -d --force-recreate
+
+docker ps
